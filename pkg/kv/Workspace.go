525962055e1ebdbb11f65fb364333c174d31bd12
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-06-23 18:04:09 +0530

    Sarthak | Adds code level documentation for Workspace

--- pkg/kv/Workspace.go
+++ pkg/kv/Workspace.go
@@ -8,12 +8,15 @@ import (
 // Workspace
 // TODO: Check if we need any locks (during get), because put/delete will happen serially on the commit of a transaction;
 // TODO: but get can run concurrently.
+// Workspace is an abstraction that deals with active and all the immutable memtables.
 type Workspace struct {
 	activeMemTable     *mvcc.MemTable
 	immutableMemTables []*mvcc.MemTable
 	options            *option.Options
 }
 
+// NewWorkspace creates a new instance of Workspace.
+// Returns an error if the creation of NewMemtable fails.
 func NewWorkspace(options *option.Options) (*Workspace, error) {
 	memtable, err := mvcc.NewMemTable(0, options)
 	if err != nil {
@@ -25,6 +28,9 @@ func NewWorkspace(options *option.Options) (*Workspace, error) {
 	}, nil
 }
 
+// PutOrUpdate puts or updates the key and the value pair in the active memtable.
+// It ensures that the memtable has the space to accommodate the incoming Key/Value pair.
+// Refer to IsFull() method inside tinydb/pkg/kv/mvcc.MemTable.
 func (workspace *Workspace) PutOrUpdate(key mvcc.VersionedKey, value mvcc.Value) error {
 	if err := workspace.ensureRoom(); err != nil {
 		return err
@@ -32,6 +38,10 @@ func (workspace *Workspace) PutOrUpdate(key mvcc.VersionedKey, value mvcc.Value)
 	return workspace.activeMemTable.PutOrUpdate(key, value)
 }
 
+// Delete deletes the key from the active memtable.
+// Deletion is not a physical deletion, it is another put with a version number.
+// It ensures that the memtable has the space to accommodate the incoming Key/Value pair.
+// Refer to IsFull() method inside tinydb/pkg/kv/mvcc.MemTable.
 func (workspace *Workspace) Delete(key mvcc.VersionedKey) error {
 	if err := workspace.ensureRoom(); err != nil {
 		return err
@@ -39,6 +49,10 @@ func (workspace *Workspace) Delete(key mvcc.VersionedKey) error {
 	return workspace.activeMemTable.Delete(key)
 }
 
+// Get returns a pair of (ValueWithVersion, bool) for the incoming key.
+// It returns (ValueWithVersion, true) if the value exists for the incoming key, else (nil, false).
+// It searches the active memtable and all the immutable memtables from the last index to 0 and tries
+// to find the key with the closest version.
 func (workspace *Workspace) Get(key mvcc.VersionedKey) (mvcc.ValueWithVersion, bool) {
 	valueWithMaxVersion := mvcc.EmptyValueWithZeroVersion()
 	for _, memtable := range workspace.allMemtables() {
@@ -59,6 +73,9 @@ func (workspace *Workspace) Get(key mvcc.VersionedKey) (mvcc.ValueWithVersion, b
 	return valueWithMaxVersion, false
 }
 
+// ensureRoom ensures that the active memtable has the room to accommodate the incoming key/value pair.
+// If the active memtable is full, a new memtable is created and the previously active memtable is added to the list of immutable memtables.
+// TODO: Send the active memtable to be written to disk
 func (workspace *Workspace) ensureRoom() error {
 	if !workspace.activeMemTable.IsFull() {
 		return nil
@@ -72,6 +89,10 @@ func (workspace *Workspace) ensureRoom() error {
 	return nil
 }
 
+// allMemtables returns a slice of all the memtables includes: the currently active memtable and all the immutable memtables.
+// the currently active memtable is placed in the index 0 of the allMemtables slice
+// all the other immutable memtables are placed in the order of the latest immutable memtable first to
+// the oldest immutable memtable last in the allMemtables slice.
 func (workspace *Workspace) allMemtables() []*mvcc.MemTable {
 	allMemtables := make([]*mvcc.MemTable, 1+len(workspace.immutableMemTables))
 	allMemtables[0] = workspace.activeMemTable
@@ -85,6 +106,7 @@ func (workspace *Workspace) allMemtables() []*mvcc.MemTable {
 	return allMemtables
 }
 
+// removeAllWAL removes the WAL of all the memtables. It is ONLY used from tests.
 func (workspace *Workspace) removeAllWAL() {
 	workspace.activeMemTable.RemoveWAL()
 	for _, memtable := range workspace.immutableMemTables {
